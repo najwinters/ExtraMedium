4279b501f965d6489b5ae5b35c47a66691c88f42
najwinters <najwinters@gmail.com>
2016-04-01 11:12:00 -0600

    buttons

---
+++ DiatonicOctopotato/DiatonicOctopotato/MultiplyChoice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiatonicOctopotato
+{
+    public partial class MultiplyChoice : Form
+    {
+        public MultiplyChoice()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            lblQuestion.Text = "What is your favorite color";
+        }
+    }
+}
