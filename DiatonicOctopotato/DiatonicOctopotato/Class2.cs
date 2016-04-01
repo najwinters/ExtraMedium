8b2d51279dd9bd69b55526c7e5ee109bc0fd4d93
najwinters <najwinters@gmail.com>
2016-04-01 14:12:41 -0600

    static-be-gone pt. 1

--- DiatonicOctopotato/DiatonicOctopotato/Class2.cs
+++ DiatonicOctopotato/DiatonicOctopotato/Class2.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace DiatonicOctopotato
 {
-    static class Assignment
+    class Assignment
     {
+        public Assignment()
+        {
 
+        }
         static string[,] list = new string[40, 2];
 
         public static void Save(string value, int i, int j)
