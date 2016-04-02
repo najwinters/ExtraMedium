b84f733169ee6c92c6edf0d8385e08eb21586ac8
Rion1337 <ryn.decker@gmail.com>
2016-04-01 23:05:35 -0600

    FILE IO F*CKING DONE!

--- DiatonicOctopotato/DiatonicOctopotato/Management.xaml.cs
+++ DiatonicOctopotato/DiatonicOctopotato/Management.xaml.cs
@@ -96,6 +96,7 @@ namespace DiatonicOctopotato
                             curAssignment.incTotal();
                         }
                     }
+                    sr.Close();
                 }
             }
             loadAssignmentList();
@@ -103,7 +104,27 @@ namespace DiatonicOctopotato
 
         private void ExportListButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if(studyLists.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select an assignment first!");
+            }
+            else
+            {
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                if(saveDialog.ShowDialog() == true)
+                {
+                    using (StreamWriter sw = new StreamWriter(saveDialog.FileName))
+                    {
+                        Assignment curAssignment = AssignmentList.getAssignment(studyLists.SelectedIndex);
+                        sw.WriteLine(curAssignment.getName());
+                        for(int i = 0; i < curAssignment.getTotal(); i++)
+                        {
+                            sw.WriteLine(curAssignment.GetList(i, 0) + "\t" + curAssignment.getList(i, 1));
+                        }
+                        sw.Close();
+                    }
+                }
+            }
         }
 
         private void removeList_Click(object sender, RoutedEventArgs e)
